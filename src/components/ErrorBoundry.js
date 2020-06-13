89c5b83f4305f1043b0ef1c3155d3ab004cdd527
Shuai Xu <sxu003@gold.ac.uk>
2020-06-13 15:27:09 +0100

    updata-redux

--- src/components/ErrorBoundry.js
+++ src/components/ErrorBoundry.js
@@ -1,20 +1,21 @@
-import React,{ Component } from 'react';
+import React, { Component } from 'react'
 
-class ErrorBoundry extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      hasError: false
-    }
+class ErrorBoundary extends Component {
+  constructor (props) {
+    super(props)
+    this.state = { hasError: false }
   }
 
-  compoentsDidCatch(error, info){
-    this.setState({hasError: true})
+  componentDidCatch (error, info) {
+    this.setState({ hasError: true })
   }
-  rendr(){
-    if(this.state.hasError){
-      return <h1>Ooooops. This is not good </h1>
+
+  render () {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>
     }
     return this.props.children
   }
 }
+
+export default ErrorBoundary
